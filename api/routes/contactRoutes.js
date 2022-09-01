a0a46872e5bf7a79f8cb425bdd16704b2bc72c14
Jimmy Martin <jimmy.martin952@gmail.com>
2022-09-01 16:28:04 +0200

    update OK

--- api/routes/contactRoutes.js
+++ api/routes/contactRoutes.js
@@ -3,40 +3,50 @@ const contactModel = require("../models/contactsModel");
 const router = express.Router();
 
 router.post("/contact", async (request, response) => {
-  // console.log(request.body);
   const contact = new contactModel(request.body);
 
   try {
     await contact.save();
-    response.send(contact);
+    response.status(201).send(contact);
   } catch (error) {
     response.status(500).send(error);
   }
 });
 
 router.get("/contact", async (request, response) => {
-  const firstname = request.query.firstname;
-  const lastname = request.query.lastname;
-  const email = request.query.email;
-  const phone_number = request.query.phone_number;
+  if (Object.keys(request.query).length !== 0) {
+    const firstname = request.query.firstname || 0;
+    const lastname = request.query.lastname || 0;
+    const email = request.query.email || 0;
+    const phone_number = request.query.phone_number || -1;
 
-  // const contacts = await contactModel.find({
-  //   firstname: firstname,
-  //   lastname: lastname,
-  // });
+    var contacts = await contactModel.find({
+      $or: [{
+        firstname: new RegExp(firstname)
+      }, {
+        lastname: new RegExp(lastname)
+      }, {
+        email: new RegExp(email)
+      }, {
+        phone_number: new RegExp(phone_number)
+      }]
+    });
 
-  const contacts = await contactModel.find({
-    $or: [{
-      firstname: firstname
-    }, {
-      lastname: lastname
-    }, {
-      email: email
-    }]
-  });
+  } else {
+    var contacts = await contactModel.find({});
+  }
 
   try {
     response.send(contacts);
+  } catch (error) {
+    response.status(500).send(error);
+  }
+});
+
+router.post("/contact/:id", async (request, response) => {
+  try {
+    const contact = await contactModel.findByIdAndUpdate(request.params.id, request.body);
+    response.send(await contactModel.findById(request.params.id));
   } catch (error) {
     response.status(500).send(error);
   }
